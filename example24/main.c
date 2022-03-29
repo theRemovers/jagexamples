b2021b49fd76a07cc24d06c66d1bc806bc675c6b
Seb/The Removers <SebRmv@jagware.org>
2022-03-29 21:29:47 +0200

    Work on new example.

---
+++ example24/main.c
@@ -0,0 +1,24 @@
+#include <jagdefs.h>
+#include <jagtypes.h>
+
+#include <stdlib.h>
+
+#include <interrupt.h>
+#include <display.h>
+#include <sprite.h>
+
+#include <console.h>
+
+int main(int argc, char *argv[]) {
+  TOMREGS->vmode = RGB16|CSYNC|BGEN|PWIDTH4|VIDEN;
+  init_interrupts();
+  init_display_driver();
+
+  display *d = new_display(0);
+
+  d->x = 16;
+
+  while(1) {
+    vsync();
+  }
+}
